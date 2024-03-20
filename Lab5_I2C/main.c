b6e9d5b0ff4c6e8a932cef1256065f79273b6b50
aymanhab1b <158302493+aymanhab1b@users.noreply.github.com>
2024-03-20 01:57:28 -0600

    Update main.c

--- Lab5_I2C/main.c
+++ Lab5_I2C/main.c
@@ -18,7 +18,7 @@
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
-volatile char a;
+volatile uint16_t axes_en = 0;
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
 
@@ -60,11 +60,185 @@ void SystemClock_Config(void);
   * @brief  The application entry point.
   * @retval int
   */
+int Gyro_Y()
+{
+	I2C2->CR2 &= ~(0xFF<<16); //Nbytes clear
+	uint16_t low_bit_y = 0;
+	uint16_t high_bit_y = 0;
+	int16_t combo_reg_y = 0;
+	I2C2->CR2 |= (0x69 << 1); //slave address
+	I2C2->CR2 |= (1<<16); //Nbytes set to one
+	I2C2->CR2 &= ~(1<<10); // write mode
+	I2C2->CR2 |= (1<<13); //Start transaction
+	while((((I2C2->ISR & (1<<4)) == 0) & ((I2C2->ISR & (1<<1)) == 0)))
+		{
+			if ((I2C2->ISR & (1<<4)) == 1)
+			{
+				GPIOC->ODR |=(1<<6);
+				continue;
+			}
+			else if ((I2C2->ISR & (1<<1)) == 1)
+			{
+			break;
+			}
+		}
+		I2C2->TXDR = (0xAA << 0); // sending sub-address
+		while ((I2C2->ISR & (1<<6)) == 0) //Checking TC
+			{				
+			}
+		I2C2->CR2 &= ~(0xFF<<16); //Nbytes clear
+		I2C2->CR2 |= (0x69 << 1); //slave address
+		I2C2->CR2 |= (2<<16); //Nbytes set to two
+		I2C2->CR2 |= (1<<10); // read mode
+		I2C2->CR2 |= (1<<13); //Start transaction
+		while((((I2C2->ISR & (1<<2)) == 0) & ((I2C2->ISR & (1<<4)) == 0))) //RXNE wait
+		{
+			if ((I2C2->ISR & (1<<4)) == 1)
+			{
+				GPIOC->ODR |=(1<<6);
+				continue;
+			}
+			else if ((I2C2->ISR & (1<<2)) == 1)
+			{
+				break;
+			}
+		}
+		low_bit_y |= (I2C2->RXDR);
+		while((((I2C2->ISR & (1<<2)) == 0) & ((I2C2->ISR & (1<<4)) == 0))) //RXNE wait
+		{
+			if ((I2C2->ISR & (1<<4)) == 1)
+			{
+				GPIOC->ODR |=(1<<6);
+				continue;
+			}
+			else if ((I2C2->ISR & (1<<2)) == 1)
+			{
+				break;
+			}
+		}
+		while ((I2C2->ISR & (1<<6)) == 0) //Checking TC
+			{				
+			}
+			I2C2->CR2 |= (1<<14); // STOP condition
+		high_bit_y |= (I2C2->RXDR);
+		combo_reg_y |= (high_bit_y <<8);
+		combo_reg_y |= low_bit_y;
+		return combo_reg_y;
+}
+int Gyro_X()
+{
+	I2C2->CR2 &= ~(0xFF<<16); //Nbytes clear
+	uint16_t low_bit_x = 0;
+	uint16_t high_bit_x = 0;
+	int16_t combo_reg_x = 0;
+	I2C2->CR2 |= (0x69 << 1); //slave address
+	I2C2->CR2 |= (1<<16); //Nbytes set to one
+	I2C2->CR2 &= ~(1<<10); // write mode
+	I2C2->CR2 |= (1<<13); //Start transaction
+	while((((I2C2->ISR & (1<<4)) == 0) & ((I2C2->ISR & (1<<1)) == 0)))
+		{
+			if ((I2C2->ISR & (1<<4)) == 1)
+			{
+				GPIOC->ODR |=(1<<6);
+				continue;
+			}
+			else if ((I2C2->ISR & (1<<1)) == 1)
+			{
+			break;
+			}
+		}
+		I2C2->TXDR = (0xA8 << 0); // sending sub-address
+		while ((I2C2->ISR & (1<<6)) == 0) //Checking TC
+			{				
+			}
+		I2C2->CR2 &= ~(0xFF<<16); //Nbytes clear
+		I2C2->CR2 |= (0x69 << 1); //slave address
+		I2C2->CR2 |= (2<<16); //Nbytes set to two
+		I2C2->CR2 |= (1<<10); // read mode
+		I2C2->CR2 |= (1<<13); //Start transaction
+		while((((I2C2->ISR & (1<<2)) == 0) & ((I2C2->ISR & (1<<4)) == 0))) //RXNE wait
+		{
+			if ((I2C2->ISR & (1<<4)) == 1)
+			{
+				GPIOC->ODR |=(1<<6);
+				continue;
+			}
+			else if ((I2C2->ISR & (1<<2)) == 1)
+			{
+				break;
+			}
+		}
+		low_bit_x |= (I2C2->RXDR);
+		while((((I2C2->ISR & (1<<2)) == 0) & ((I2C2->ISR & (1<<4)) == 0))) //RXNE wait
+		{
+			if ((I2C2->ISR & (1<<4)) == 1)
+			{
+				GPIOC->ODR |=(1<<6);
+				continue;
+			}
+			else if ((I2C2->ISR & (1<<2)) == 1)
+			{
+				break;
+			}
+		}
+		high_bit_x |= (I2C2->RXDR);
+		while ((I2C2->ISR & (1<<6)) == 0) //Checking TC
+			{				
+			}
+		I2C2->CR2 |= (1<<14); // STOP condition
+		combo_reg_x |= (high_bit_x <<8);
+		combo_reg_x |= low_bit_x;
+		return combo_reg_x;
+}
+		
+		
+	
+void Gyro_Init()
+{
+		I2C2->CR2 &= ~(0xFF<<16); //Nbytes clear
+		I2C2->CR2 |= (0x69 << 1); //slave address
+		I2C2->CR2 |= (2<<16); //Nbytes set to two
+		I2C2->CR2 &= ~(1<<10); // Write mode
+		I2C2->CR2 |= (1<<13); //Start transaction
+		while((((I2C2->ISR & (1<<4)) == 0) & ((I2C2->ISR & (1<<1)) == 0)))
+		{
+			if ((I2C2->ISR & (1<<4)) == 1)
+			{
+				GPIOC->ODR |=(1<<6);
+				continue;
+			}
+			else if ((I2C2->ISR & (1<<1)) == 1)
+			{
+			break;
+			}
+		}
+		I2C2->TXDR = (0x20 << 0); // sending sub-address
+		while((((I2C2->ISR & (1<<4)) == 0) & ((I2C2->ISR & (1<<1)) == 0))) // Re-checking NACKF and TXIS
+		{
+			if ((I2C2->ISR & (1<<4)) == 1)
+			{
+				GPIOC->ODR |=(1<<6);
+				continue;
+			}
+			else if ((I2C2->ISR & (1<<1)) == 1)
+			{
+			break;
+			}
+		}
+		I2C2->TXDR = (0xb << 0); //sending data
+		while ((I2C2->ISR & (1<<6)) == 0) //Checking TC
+			{				
+			}
+			I2C2->CR2 |= (1<<14); // STOP condition
+}
+
+
+	
 void I2C_read()
 {
-		I2C2->CR2 |= (0x69 << 1);
+		I2C2->CR2 |= (0x69 << 1); //re-initalization
 		I2C2->CR2 |= (1<<16);
-		I2C2->CR2 |= (1<<10);
+		I2C2->CR2 |= (1<<10); // read mode
 		I2C2->CR2 |= (1<<13);
 	while((((I2C2->ISR & (1<<2)) == 0) & ((I2C2->ISR & (1<<4)) == 0)))
 	{
@@ -118,6 +292,8 @@ void I2C_write()
 int main(void)
 {
   /* USER CODE BEGIN 1 */
+		HAL_Init();
+		SystemClock_Config();
 		// Enable GPIOC clock
     RCC->AHBENR |= RCC_AHBENR_GPIOCEN;
 		// Enable GPIOB clock
@@ -156,6 +332,8 @@ int main(void)
 		GPIOC->ODR &= ~(1<<7);
 		GPIOC->ODR &= ~(1<<8);
 		GPIOC->ODR &= ~(1<<9);
+		//Enabling X, Y axes
+		
   /* USER CODE END 1 */
 
   /* MCU Configuration--------------------------------------------------------*/
@@ -183,12 +361,54 @@ int main(void)
   /* USER CODE BEGIN WHILE */
   while (1)
   {
+		int16_t x_rot;
+		int16_t y_rot;
     /* USER CODE END WHILE */
-		I2C_write();
+		//I2C_write();
+		Gyro_Init();
+		HAL_Delay(1);
+		x_rot = Gyro_X();
+		HAL_Delay(1);
+		y_rot = Gyro_Y();
+		HAL_Delay(1);
+		int16_t lower_thresh = -125;
+		int16_t upper_thresh = 125;
+		if (x_rot < lower_thresh)
+		{
+			GPIOC->ODR |= (1<<8); // POSITIVE X TURN ORANGE
+			GPIOC->ODR &=~ (1<<9); // OFF GREEN
+		}
+		else if (x_rot > upper_thresh)
+		{
+			GPIOC->ODR |= (1<<9); // Negative X TURN green
+			GPIOC->ODR &=~ (1<<8); // OFF orange
+		}
+		else
+		{
+			GPIOC->ODR &=~ (1<<9); // OFF green
+			GPIOC->ODR &=~ (1<<8); // OFF orange
+		}
+		if (y_rot < lower_thresh)
+		{
+			GPIOC->ODR |= (1<<7); // POSITIVE Y TURN RED
+			GPIOC->ODR &=~ (1<<6); // OFF BLUE
+		}
+		else if (y_rot > upper_thresh)
+		{
+			GPIOC->ODR |= (1<<6); // Negative Y TURN BLUE
+			GPIOC->ODR &=~ (1<<7); // OFF RED
+		}
+		else
+		{
+			GPIOC->ODR &=~ (1<<6); // OFF red
+			GPIOC->ODR &=~ (1<<7); // OFF blue
+		}	
+		HAL_Delay(97);
+	}		
+			
     /* USER CODE BEGIN 3 */
-  }
-  /* USER CODE END 3 */
 }
+  /* USER CODE END 3 */
 
 /**
   * @brief System Clock Configuration
